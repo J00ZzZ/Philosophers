d95caad96b3caa878b666ec8f150c4c3af579232
J00ZzZ <liaujoel1117@gmail.com>
2024-12-24 17:28:34 +0800

    modified header file

--- inc/philo.h
+++ inc/philo.h
@@ -6,22 +6,23 @@
 /*   By: liyu-her <liyu-her@student.42.kl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 04:23:06 by liyu-her          #+#    #+#             */
-/*   Updated: 2024/12/10 02:33:54 by liyu-her         ###   ########.fr       */
+/*   Updated: 2024/12/24 17:27:50 by liyu-her         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <sys/wait.h>
-# include <errno.h>
-# include <fcntl.h>
-# include <limits.h>
-# include <stdarg.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <string.h>
 # include <unistd.h>
+# include <stdbool.h>
 # include <pthread.h>
+# include <sys/time.h>
+# include <limits.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stdarg.h>
+# include <string.h>
 
 #endif
